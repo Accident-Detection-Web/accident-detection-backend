5a6d693b5c4375ad800800216b99d9b5d69ca9e3
Inhee <whdlsgml1599@gmail.com>
2024-04-02 22:06:40 +0900

    docs: ê¸basic setting

---
+++ src/main/java/com/example/accidentdetectionservice/AccidentDetectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.accidentdetectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentDetectionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccidentDetectionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accidentdetectionservice/AccidentDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accidentdetectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccidentDetectionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
