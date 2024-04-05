374230669e03ca6b8ca7ef18d5907c9a57424c2f
Inhee <whdlsgml1599@gmail.com>
2024-04-06 01:42:22 +0900

    feat : 회원가입시 이메일 인증 기능 추가

---
+++ src/main/java/com/example/accidentdetectionservice/domain/user/controller/MailController.java
@@ -0,0 +1,20 @@
+package com.example.accidentdetectionservice.domain.user.controller;
+
+import com.example.accidentdetectionservice.domain.user.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/mail")
+    public String mailSend(String mail) {
+        int number = mailService.sendMail(mail);
+        return "" + number;
+    }
+
+}
