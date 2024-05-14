89ce63de5030642e02755e2039d59aa7d16c5755
InHeeCho <whdlsgml1599@naver.com>
2024-05-14 15:22:44 +0900

    comment : @apiNote 설명 추가

--- src/main/java/com/example/accidentdetectionservice/domain/notify/controller/NotifyController.java
+++ src/main/java/com/example/accidentdetectionservice/domain/notify/controller/NotifyController.java
@@ -19,6 +19,13 @@ public class NotifyController {
 
     private final NotifyService notifyService;
 
+    /**
+     * @apiNote 1.클라이언트가 로그인 시점에 해당 api 를 호출하여 SseEmitter 객체에 구독
+     * @apiNote 2.Flask Server 로 부터 데이터를 받는 시점 Consumer Server 에서 알림 객체가 생성 및 알림 생성후 프론트에게 자동 전달
+     * @param userDetails
+     * @param lastEventId
+     * @return
+     */
     @GetMapping(value = "/subscribe", produces = "text/event-stream")
     public ResponseEntity<SseEmitter> subscribe(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                 @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
