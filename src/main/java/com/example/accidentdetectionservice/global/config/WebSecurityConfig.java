665f257cdeb9ed708d2b76cda4a026e2310171a8
InHeeCho <whdlsgml1599@naver.com>
2024-05-30 16:17:27 +0900

    refactor : WebSecurityConfig 수정

--- src/main/java/com/example/accidentdetectionservice/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/accidentdetectionservice/global/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig {
                 "https://capstone-2024-frontend-only.vercel.app",
                 "https://backend-capstone.site",
                 "http://backend-capstone.site:8080",
-                "http://s13-mynextjs.ap-northeast-2.elasticbeanstalk.com",
+                "http://s14-mynextjs.ap-northeast-2.elasticbeanstalk.com",
                 "http://capstone-aiserver.shop",
                 "*"
         ));
